4789c371bcdedc063f9d2337693d5460d5382966
Козырев <kigl@yandex.ru>
2022-08-31 11:11:27 +0500

    Добавьте файлы проекта.

---
+++ ShopPay/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopPay.Startup))]
+namespace ShopPay
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
